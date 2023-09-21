414220e45b7cbd8cc897d638ff6d4bd1cba6dda3
Shubham-2323 <120272863+Shubham-2323@users.noreply.github.com>
2023-09-21 15:57:46 +0530

    1 file modified

---
+++ 4-GuessTheNumber/chaiaurcode.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form');
+
+const Numb = Math.floor(Math.random() * 100 + 1);
+
+const submit = document.querySelector('#subt');
+const result = document.querySelector('#lastResult');
+const guess = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const lowOrHigh = document.querySelector('#lowOrHigh');
+const paras = document.querySelector('#resultParas');
